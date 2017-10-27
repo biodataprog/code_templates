96f48fa7e75f40b67feb2b951ef1982baad49d4d
Jason Stajich <jason.stajich@ucr.edu>
2017-10-27 13:43:58 -0700

    pattern match example 1

---
+++ Regexp/patterns_group.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import re
+
+m = re.search("((AB)+)C","ABABABCDED")
+if m:
+    print("Group 0",m.group(0))
+    print("Group 1",m.group(1))
+    print("Group 2",m.group(2))
+    
