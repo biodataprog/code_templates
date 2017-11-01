c0db800e51c7222048f3dd5f30b193ea9ea890a4
Jason Stajich <jason.stajich@ucr.edu>
2017-11-01 13:59:42 -0700

    feature printing

---
+++ BioPython/03_parse_seq_print_features.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+import Bio
+from Bio import SeqIO
+from Bio.Seq import Seq
+    
+
+# seqfile 
+if len(sys.argv) != 2:
+    print("argument should be\n","02_parse_seq.py AJ240084.gbk")
+    exit()
+
+filename = sys.argv[1]
+for seq_record in SeqIO.parse( filename , "genbank"):
+    print(seq_record.id)
+    for feature in seq_record.features:
+        print("\t",feature.type,feature.location)
+        print("\t",feature.type,feature.location.start, feature.location.end, feature.location.strand)
