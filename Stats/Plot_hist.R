6b5da1057c185f60eb2d1c6c36d49e751b392000
Jason Stajich <jason.stajich@ucr.edu>
2017-11-17 13:32:58 -0800

    flip filename

--- Stats/Plot_hist.R
+++ Stats/Plot_hist.R
@@ -1,14 +1,15 @@
-mRNA <- read.csv("mRNA.lengths",sep="\t",header=T)
-TE <- read.csv("transposon.lengths",sep="\t",header=T)
+mRNA = read.csv("mRNA.lengths",sep="\t",header=T,row.names=1)
+TE = read.csv("transposon.lengths",sep="\t",header=T,row.names=1)
+summary(mRNA)
 summary(TE)
-
 pdf("lengths_hist.pdf")
 hist(TE$Length)
 hist(TE$Length,100,main="TE length distribution")
 hist(mRNA$Length,100,main="mRNA length distribution")
 
 short = subset(mRNA,mRNA$Length < 10000)
+summary(short)
 hist(short$Length,100,main="shorter mRNA length distribution")
-big_transcripts = subset(mRNA,mRNA$Length > 50000)
-big_transcripts$Name
-big_transcripts
+long = subset(mRNA,mRNA$Length > 50000)
+summary(long)
+long
--- Stats/download.sh
+++ Stats/download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-curl ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/transcripts/c_elegans.PRJNA13758.WS260.mRNA_transcripts.fa.gz > transposon.fa.gz
+curl ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/transcripts/c_elegans.PRJNA13758.WS260.mRNA_transcripts.fa.gz > mRNA.fa.gz
 
-curl ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/transcripts/c_elegans.PRJNA13758.WS260.transposon_transcripts.fa.gz > mRNA.fa.gz
+curl ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/transcripts/c_elegans.PRJNA13758.WS260.transposon_transcripts.fa.gz > transposon.fa.gz
