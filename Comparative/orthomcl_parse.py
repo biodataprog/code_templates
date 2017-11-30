fa0a208a371f05739d09a8be75555908abca2b2d
Jason Stajich <jason.stajich@ucr.edu>
2017-11-29 16:04:26 -0800

    ortho

--- Comparative/orthomcl_parse.py
+++ Comparative/orthomcl_parse.py
@@ -27,12 +27,19 @@ header.append(species_names.keys())
 print("\t".join(map(str,header)))
 
 orthonumber = 1
+# orthologs is a list of dictionaries (one per line)
+# ortholog is a dictionary of counts
+# SP1: 10
+# SP2: 5
 for ortholog in orthologs:
-    final = ["ORTHO_%d"%(orthonumber)]    
+    final = ["ORTHO_%05d"%(orthonumber)]    
+    # walk through each species name, in the same order each time
     for species in species_names:
+        # lookup the gene count in this orthology, for each species 
         if species in ortholog:
             final.append(ortholog[species])
         else:
             final.append(0)
+    # print final as one line
     print("\t".join(map(str,final)))
     orthonumber += 1
