9de9463e7b61b13b6fab321f96d777a22f8eab50
Jason Stajich <jasonstajich.phd@gmail.com>
2017-12-07 19:54:16 -0800

    variant calling

---
+++ PopGen/02_call_variants.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#SBATCH --nodes 1 --ntasks 8 --time 12:00:00 --mem 16G
+
+module load bcftools
+module load samtools
+
+module load tabix
+
+if [ ! -f Ecoli.vcf ]; then
+ bcftools mpileup -Ou -f E_coli_K12.fa DnaK_42C.bam | bcftools call -vmO z -o Ecoli.vcf.gz
+fi
+
+# This VCF file is the locations of the SNPs and INDELs
+zcat Ecoli.vcf.gz > Ecoli.vcf
+tabix -p vcf Ecoli.vcf.gz
